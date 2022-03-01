8fe5bd94d7108697ae9ac646d45f2a3d7cc912eb
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2022-03-02 00:35:26 +0600

    fix: search spinner id

---
+++ assets/js/index.js
@@ -0,0 +1,4 @@
+/* Search Section */
+const searchBar = document.getElementById('search-input');
+const searchBarBtn = document.getElementById('search-btn');
+const searchBarSpinner = document.getElementById('search-spinner');
