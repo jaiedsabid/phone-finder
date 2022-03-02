d944c3b100ea41ac8367a1ebddf5756799a86ca4
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2022-03-02 19:27:03 +0600

    Added slide-over to display phone details

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,6 +18,21 @@ const phonesGrid = document.getElementById('phones-grid');
 const loadMoreBtn = document.getElementById('load-more-btn');
 const loadMoreBtnSpinner = document.getElementById('load-more-spinner');
 
+/* Slider */
+const slider = document.getElementById('slider');
+const toggleSliderBtn = document.getElementById('toggle-slider-btn');
+
+/* Slider Displays */
+const sliderTitle = document.getElementById('slider-title');
+const phoneImage = document.getElementById('phone-image');
+const phoneBrand = document.getElementById('phone-brand');
+const phoneReleaseDate = document.getElementById('release-date');
+const phoneStorage = document.getElementById('storage');
+const phoneDisplaySize = document.getElementById('display-size');
+const phoneChipset = document.getElementById('chipset');
+const phoneMemory = document.getElementById('memory');
+const phoneSensors = document.getElementById('sensors');
+const otherFeatures = document.getElementById('other-features');
 
 /* Functions */
 
@@ -34,6 +49,17 @@ const toggleLoadMoreSpinner = () => {
 /* Toggle load more button */
 const toggleLoadMoreBtn = () => {
     loadMoreBtn.classList.toggle('hidden');
+}
+
+/* Toggle slider */
+const toggleSlider = () => {
+    if ( slider.classList.contains('slide-enter') ) {
+        slider.classList.remove('slide-enter');
+        slider.classList.add('slide-leave');
+    } else {
+        slider.classList.remove('slide-leave');
+        slider.classList.add('slide-enter');
+    }
 }
 
 /* Reset toggle button state */
@@ -67,11 +93,17 @@ const onClickLoadMore = () => {
 
 const search = async () => {
     try {
+        const searchTerm = searchBarInput.value;
+
+        // Return if search term is empty
+        if ( ! searchTerm.length ) {
+            return;
+        }
+
         toggleSearchSpinner();
         resetPhonesGrid();
         resetPhoneAndLoadMoreState();
 
-        const searchTerm = searchBarInput.value;
         const response = await fetch(searchAPI + searchTerm);
         const phones = await response.json();
         
@@ -138,7 +170,7 @@ const phoneItemHTMLGenerator = (phone) => {
             </div>
             <div
                 class="absolute bottom-2 right-0 left-0 mt-4 h-16 flex justify-center p-2 invisible group-hover:visible">
-                <button id="view-details" type="button"
+                <button id="${slug}" type="button" onclick="renderFeatures('${slug}')"
                     class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none transition duration-200 ease-in-out">View
                     Details</button>
             </div>
@@ -172,6 +204,58 @@ const renderPhones = (phones) => {
     });
 }
 
+const renderFeatures = async (slug) => {
+    try {
+        const response = await fetch(detailsAPI + slug);
+        const features = await response.json();
+
+        if (!!features.status) {
+            const {mainFeatures, name, releaseDate, brand, image, others} = features.data;
+
+            // Set basic info
+            sliderTitle.innerText = name;
+            phoneImage.src = image;
+            phoneBrand.innerText = brand;
+            phoneReleaseDate.innerText = releaseDate;
+
+            // Set main features
+            const { storage, displaySize, chipSet, memory, sensors } = mainFeatures;
+            phoneStorage.innerText = storage;
+            phoneDisplaySize.innerText = displaySize;
+            phoneChipset.innerText = chipSet;
+            phoneMemory.innerText = memory;
+
+            // Set sensors
+            phoneSensors.innerHTML = ''; // Reset first
+            sensors.forEach((sensor) => {
+                const listElement = document.createElement('li');
+                listElement.innerText = sensor;
+                phoneSensors.appendChild(listElement);
+            });
+
+            // Set others
+            otherFeatures.innerHTML = ''; // Reset first
+            for(const key in others) {
+                otherFeaturesHTMLGenerate(key, others[key]);
+            }
+
+            // Open Slider
+            toggleSlider();
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const otherFeaturesHTMLGenerate = (key, value) => {
+    otherFeatures.insertAdjacentHTML('beforeend', `
+        <tr>
+            <td class="text-sm font-semibold inline-block w-28">${key}: </td>
+            <td class="text-sm font-medium inline-block">${value}</td>
+        </tr>
+    `);
+}
+
 
 /* Event-listeners */
 searchBarBtn.addEventListener('click', search);
@@ -182,6 +266,7 @@ searchBarInput.addEventListener('keypress', event => {
         search();
     }
 });
+toggleSliderBtn.addEventListener('click', toggleSlider);
 /* Fix initial height */
 window.addEventListener('DOMContentLoaded', () => {
     const header = document.getElementsByTagName('header')[0];
